abed45e4950c6ddd17f866f4519d285da5e46931
Senk111 <akash.baral111@gmail.com>
2023-12-16 12:57:45 +0530

    1st Push

---
+++ Main3Jenkins/src/test/java/main3/AddCompanyTest.java
@@ -0,0 +1,14 @@
+package main3;
+
+import org.testng.annotations.Test;
+
+public class AddCompanyTest 
+{
+	@Test
+	public void addCompanyTest() 
+	{
+		System.out.println("Company: Alphabets");
+		
+	}
+
+}
---
+++ Main3Jenkins/src/test/java/main3/CreateOrganisationTest.java
@@ -0,0 +1,14 @@
+package main3;
+
+import org.testng.annotations.Test;
+
+public class CreateOrganisationTest 
+{
+	@Test
+	public void createOrganisationTest() 
+	{
+		System.out.println("Organisation Name : G@rgoyl3$");
+		
+	}
+
+}
